849d746a1dd35b337693769eb601eba2f280d526
ShunyaSuzuki <suzuki@lamp.jp>
2021-11-09 11:19:16 +0900

    add main branch

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -7,6 +7,11 @@ if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
 
+else if [ ["$VERCEL_GIT_COMMIT_REF" == "main"] ] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
 else
   # Don't build
   echo "🛑 - Build cancelled"
