0d268e9d543e90b96511651fea410ae57532f4ee
ShunyaSuzuki <suzuki@lamp.jp>
2021-11-11 00:15:03 +0900

    #7 add author avatar

--- src/components/page/post/PostCard.tsx
+++ src/components/page/post/PostCard.tsx
@@ -1,4 +1,13 @@
-import { Box, Card, CardContent, Typography, CardActionArea, Stack, Chip } from '@mui/material'
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  Stack,
+  Chip,
+} from '@mui/material'
 import { blue } from '@mui/material/colors'
 import type { NextPage } from 'next'
 import Link from 'next/link'
@@ -19,27 +28,38 @@ const PostCard: NextPage<{ post: Post }> = ({ post }) => {
             py={5}
             px={3}
             sx={{
+              position: 'relative',
               backgroundColor: blue[50],
               textAlign: 'center',
               fontSize: '5rem',
               lineHeight: 1,
             }}
           >
+            {post.properties.tags.multi_select[0].name && (
+              <Chip
+                label={post.properties.tags.multi_select[0].name}
+                color="primary"
+                size="small"
+                sx={{
+                  position: 'absolute',
+                  top: '1rem',
+                  right: '1rem',
+                  lineHeight: '1rem',
+                }}
+              />
+            )}
             <Twemoji svg text={post.icon?.emoji || '☕'} />
           </Box>
           <CardContent>
             <Typography variant="h6" component="h2" mb={3}>
               {post.properties.title.title[0].text.content}
             </Typography>
-            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1}>
-              {post.properties.tags.multi_select[0].name && (
-                <Chip
-                  label={post.properties.tags.multi_select[0].name}
-                  color="primary"
-                  size="small"
-                  sx={{
-                    lineHeight: '1rem',
-                  }}
+            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+              {post.properties.author.created_by.avatar_url && (
+                <Avatar
+                  sx={{ width: 28, height: 28 }}
+                  alt={post.properties.author.created_by.name}
+                  src={post.properties.author.created_by.avatar_url}
                 />
               )}
               {post.created_time && (
