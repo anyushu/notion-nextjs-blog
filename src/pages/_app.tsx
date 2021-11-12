ef88052f6206b308c80985bf9a53cfd5aeac1870
ShunyaSuzuki <suzuki@lamp.jp>
2021-11-12 13:28:58 +0900

    #8 add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,14 @@ import { ThemeProvider } from '@mui/material/styles'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 import TransitionProps from '../components/common/TransitionProps'
 import { defaultSeo } from '../next-seo.config'
 import createEmotionCache from '../styles/createEmotionCache'
 import theme from '../styles/theme'
 import { SearchContext } from 'context/searchContext'
+import { GTAG_ID, pageview } from 'lib/gtag'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -20,8 +22,21 @@ interface MyAppProps extends AppProps {
 }
 
 export default function MyApp(props: MyAppProps): JSX.Element {
+  const router = useRouter()
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   const [search, setSearch] = useState<string>('')
+
+  useEffect(() => {
+    if (!GTAG_ID) return
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
