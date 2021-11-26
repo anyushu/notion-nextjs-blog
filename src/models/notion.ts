94e500ce6b5d297f8da1fe937f2da205d5a62bd1
anyushu <suzuki@lamp.jp>
2021-11-26 19:03:44 +0900

    #25 textのネストブロックの対応

--- src/models/notion.ts
+++ src/models/notion.ts
@@ -71,3 +71,45 @@ export interface PostProperty {
     ]
   }
 }
+
+/**
+ * property of notion rich text
+ */
+export interface RichTextItem {
+  text: {
+    content: string
+    link?: {
+      url: string
+    } | null
+  }
+  type?: 'text'
+  annotations?: {
+    bold?: boolean
+    italic?: boolean
+    strikethrough?: boolean
+    underline?: boolean
+    code?: boolean
+    color?:
+      | 'default'
+      | 'gray'
+      | 'brown'
+      | 'orange'
+      | 'yellow'
+      | 'green'
+      | 'blue'
+      | 'purple'
+      | 'pink'
+      | 'red'
+      | 'gray_background'
+      | 'brown_background'
+      | 'orange_background'
+      | 'yellow_background'
+      | 'green_background'
+      | 'blue_background'
+      | 'purple_background'
+      | 'pink_background'
+      | 'red_background'
+  }
+  plain_text: string
+  href: string | null
+}
