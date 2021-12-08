c81cc71cde69afe4aa09c5797eac6c032656e525
anyushu <suzuki@lamp.jp>
2021-12-08 17:10:35 +0900

    fix: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,24 @@
 import { NextSeo } from 'next-seo'
 import Link from 'next/link'
+import Button from 'components/atoms/Button'
+import Container from 'components/atoms/Container'
+import { siteTitle } from 'next-seo.config'
 
 const Custom404 = () => {
   return (
     <>
       <NextSeo description="JAMstack Blog with Notion API x Next.js" />
+
+      <Container className="py-24 text-center">
+        <h1 className="text-xl tracking-widest">{siteTitle}</h1>
+        <h2 className="mt-24 text-9xl tracking-widest text-gray-300">404</h2>
+        <p className="mt-12 tracking-widest">
+          we are sorry, but the page you requested was not found.
+        </p>
+        <div className="mt-12 tracking-widest">
+          <Button href="/">Home</Button>
+        </div>
+      </Container>
     </>
   )
 }
