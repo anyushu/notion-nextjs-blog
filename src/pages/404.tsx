df7bd3519554eaec09d8a3c811a19c9c5ab4fa91
anyushu <suzuki@lamp.jp>
2021-12-09 17:24:51 +0900

    add home link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
 import { NextSeo } from 'next-seo'
+import Link from 'next/link'
 import Button from 'components/atoms/Button'
 import Container from 'components/atoms/Container'
 import { siteTitle } from 'next-seo.config'
@@ -9,10 +10,14 @@ const Custom404 = () => {
       <NextSeo description="JAMstack Blog with Notion API x Next.js" />
 
       <Container className="py-24 text-center">
-        <h1 className="text-xl tracking-widest">{siteTitle}</h1>
+        <h1 className="text-xl tracking-widest">
+          <Link href="/">
+            <a>{siteTitle}</a>
+          </Link>
+        </h1>
         <h2 className="mt-24 text-9xl tracking-widest text-gray-300">404</h2>
         <p className="mt-12 tracking-widest">
-          we are sorry, but the page you requested was not found.
+          I am sorry, but the page you requested was not found.
         </p>
         <div className="mt-12 tracking-widest">
           <Button href="/">Home</Button>
