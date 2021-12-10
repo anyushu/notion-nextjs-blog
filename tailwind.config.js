3812895214be5b00f4e52dbf9079a914403a57e5
anyushu <suzuki@lamp.jp>
2021-12-10 18:20:12 +0900

    install tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,47 +2,21 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      sans: ['Red Hat Display', 'Noto Sans JP', 'sans-serif'],
-      mono: [
-        'ui-monospace',
-        'SFMono-Regular',
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        'Liberation Mono',
-        'Courier New',
-        'monospace',
-      ],
-    },
-    colors: {
-      black: '#000',
-      white: '#fff',
-      gray: {
-        900: '#323232',
-        700: '#565656',
-        500: '#7B7B7B',
-        300: '#CCCCCC',
-        100: '#F4F4F4',
-      },
-      errors: '#E10000',
-      indigo: colors.indigo,
-      red: colors.red,
-      yellow: colors.yellow,
-      blue: colors.blue,
-      green: colors.green,
-      purple: colors.purple,
-    },
-    extend: {},
-  },
-  variants: {
     extend: {
-      textColor: ['active'],
+      fontFamily: {
+        sans: ['Red Hat Display', 'Noto Sans JP', 'sans-serif'],
+      },
+      colors: {
+        gray: colors.neutral,
+        indigo: colors.indigo,
+        red: colors.red,
+        yellow: colors.amber,
+        blue: colors.blue,
+        green: colors.emerald,
+        purple: colors.violet,
+      },
     },
   },
-  plugins: [],
 }
